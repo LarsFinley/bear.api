298677b062eb4aa570ce40d3cc40a2a942d8b1ca
Lars Finley Martinson <larsm_eyes@hotmail.com>
2016-02-15 10:37:52 -0700

    working

---
+++ myCalculator.js
@@ -0,0 +1,11 @@
+var Calculator = require('./calculator')
+
+
+var myCalculator = new Calculator();
+console.log(myCalculator);
+
+
+var myCalculator = new Calculator()
+
+myCalculator.sum (3,2);
+myCalculator.subtract (3,2);
