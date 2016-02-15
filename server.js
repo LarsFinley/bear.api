473b43c0bff1429bcf43d1fa3f608c6d3fd0655a
Lars Finley Martinson <larsm_eyes@hotmail.com>
2016-02-15 11:54:35 -0700

    bears are working

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
-
 var app = express();
 
+var monte = {name: 'monte', id: 0, age: 15, gender: 'male'};
+var ferdinand = {name: 'ferdinand', id: 1, age: 11, gender: 'male'};
+var suzy = {name: 'suzy', id: 2, age: 13, gender: 'female'}
+var murino = {name: 'murino', id: 3, age: 32, gender: 'female'}
+
+var bears = [monte, ferdinand, suzy, murino];
+
+
+
+
 app.get('/api/bears', function(req, res){
-	res.json({title: 'does it work?', body: 'I hope so...'})
+	res.json(bears)
 });
 
 
